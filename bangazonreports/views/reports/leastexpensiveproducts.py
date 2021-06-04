de84991d9b09ec1142ff864f36ba79fb6b7bbcfd
emilycentko <emily.centko@gmail.com>
2021-06-04 10:43:38 -0500

    Django ORM report for getting least expensive products under 1000

---
+++ bangazonreports/views/reports/leastexpensiveproducts.py
@@ -0,0 +1,16 @@
+"""Module for generating products priced at $1000 or more"""
+from django.shortcuts import render
+from bangazonapi.models import Product
+from bangazonreports.views import Connection
+
+
+def inexpensive_products_list(request):
+
+    least_expensive_products = Product.objects.filter(price__lte = 1000)
+
+    template = 'products/list_of_inexpensive_products.html'
+    context = {
+            'inexpensive_products_list': least_expensive_products
+        }
+
+    return render(request, template, context)
